75d99aca0c560573c242881cc0b3ebd181c369c9
Hồ Phi Long <Long171A010063@st.vhu.edu.vn>
2020-10-26 14:48:05 +0700

    Add project files.

---
+++ HoPhiLong/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using HoPhiLong.Areas.Identity.Data;
+using HoPhiLong.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(HoPhiLong.Areas.Identity.IdentityHostingStartup))]
+namespace HoPhiLong.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<HoPhiLongContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("HoPhiLongContextConnection")));
+
+                services.AddDefaultIdentity<HoPhiLongUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<HoPhiLongContext>();
+            });
+        }
+    }
+}
